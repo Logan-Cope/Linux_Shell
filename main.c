8fbf0dd6e3e88bd5f1f2bddb93146e638b98cd20
Logan Cope <copel@os1.engr.oregonstate.edu>
2021-02-02 18:02:59 -0800

    Made variable expansion function

--- main.c
+++ main.c
@@ -30,16 +30,85 @@ int processCommand(char **commands, char *command)
         commands[index] = token;
         index++;
         count++;
-
-        //     printf("%s\n", curArg->argument);
-        //     // curArg->argument = calloc(strlen(token) + 1, sizeof(char));
         //     // curArg->argument = token;
-        //     // strcpy(curArg->argument, token);
-        //     // tail->next = curArg;
-        //     // printf("%s\n", token);
         token = strtok_r(NULL, " ", &saveptr);
     }
     return count;
+}
+
+void replaceString(char **commands, int start, int index)
+{
+    // Convert pid to string. Set to 8 for max pid size of 64-bit plus null
+    char pid[8];
+    sprintf(pid, "%d", getpid());
+    // printf("%s\n", pid);
+    // printf("%d\n", strlen(pid));
+    // char newCommand[strlen(commands[index]) + strlen(pid)];
+    char *newCommand = calloc(strlen(commands[index]) + strlen(pid) + 1, sizeof(char));
+    int newIndex;
+    int i;
+    // printf("%s\n", newCommand);
+    for (newIndex = 0; newIndex < start; newIndex++)
+    {
+        newCommand[newIndex] = commands[index][newIndex];
+        // printf("%c\n", commands[index][newIndex]);
+    }
+    // newCommand[newIndex] = 0;
+    strcat(newCommand, pid);
+    char *finalPart = calloc(strlen(commands[index]) + 1, sizeof(char));
+    // char finalPart[strlen(commands[index])];
+    newIndex = 0;
+    for (i = start + 2; i < strlen(commands[index]); i++)
+    {
+        // printf("%c\n", commands[index][i]);
+
+        finalPart[newIndex] = commands[index][i];
+        newIndex++;
+    }
+    strcat(newCommand, finalPart);
+    commands[index] = newCommand;
+}
+
+void variableExpansion(char **commands, int numArguments)
+{
+    int index = 0;     // pointer to command in commands array
+    bool didExpansion; // bool to see if we need to check for another expansion
+    int i;             // pointer to characters of each command (nested for loop)
+
+    while (index < numArguments)
+    {
+        didExpansion = false;
+        for (i = 0; i < strlen(commands[index]) - 1; i++)
+        {
+            // printf("%c\n", commands[index][i]);
+            // Check if we have two '$' in a row and variable expansion is necessary
+            if (commands[index][i] == '$')
+            {
+                if (commands[index][i + 1] == '$')
+                {
+                    // printf("Changing: %s\n", commands[index]);
+                    replaceString(commands, i, index);
+                    didExpansion = true;
+                    break;
+                }
+            }
+        }
+
+        // We only want to progress pointer if we did not do an expansion
+        // otherwise, we want to check that same command again to see
+        // if it needs more expansions
+        if (!didExpansion)
+        {
+            index++;
+        }
+        fflush(stdout);
+    }
+    index = 0;
+    while (index < numArguments)
+    {
+        printf("%s\n", commands[index]);
+        index++;
+    }
 }
 
 int main(void)
@@ -61,21 +130,25 @@ int main(void)
         fgets(command, 2048, stdin);
         // If command was exit, set exitProgram boolean to true so that
         // we know to exit the program
-        if (strncmp(command, "exit", 4) == 0)
+        if (strncmp(command, "exit", 4) == 0 && strlen(command) == 5)
         {
             exitProgram = true;
         }
         else
         {
             numArguments = processCommand(commands, command);
-            // Handle blank lines or comments
+            // Handle blank lines or comments by checking if first char
+            // is '#' or a blank line, i.e. '\n'
             if (commands[0][0] == '#' || commands[0][0] == '\n')
             {
                 continue;
             }
             else
             {
-                printf("this is in 0 %c\n", commands[0][0]);
+                // printf("this is in 0 %c\n", commands[0][0]);
+                // printf("The pid is: %d\n", getpid());
+                // handle neccessary variable expansion
+                variableExpansion(commands, numArguments);
             }
             // printf("number is: %d", strlen(commands[0]));
         }
